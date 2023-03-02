35e0c6fe323047a09d5d9e9f99b3dd7c42c3e298
herloncosta <herlon36@gmail.com>
2023-03-02 20:16:25 -0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express, { json, urlencoded } from 'express'
+import cors from 'cors'
 import { router } from './routes.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: false }))
 
